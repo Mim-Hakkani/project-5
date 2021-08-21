bb8e787cdcd9edbcce94b18248f4495c3e1e76ae
Hakkani Mim <mimhakkani4g@gmail.com>
2021-08-21 11:54:49 +0600

    set up all in functional way

--- js/main.js
+++ js/main.js
@@ -1,103 +1,152 @@
 // click event for memory 
 
 document.getElementById('8-gb').addEventListener('click',function(){
-   memoryadd('8-gb'); 
+   computer_confure('memory','8'); 
 });
 
 document.getElementById('16-gb').addEventListener('click',function(){
-    memoryadd('16-gb');
+    computer_confure('memory','16');
  });
 
 
 //click event for storage unit
 
 document.getElementById('storage-1').addEventListener('click',function(){
-    ssdStorage('storage-1'); 
+    computer_confure('storage','storage-1'); 
  });
  
  document.getElementById('storage-2').addEventListener('click',function(){
-    ssdStorage('storage-2');
+    computer_confure('storage','storage-2');
   });
   document.getElementById('storage-3').addEventListener('click',function(){
-    ssdStorage('storage-3');
+    computer_confure('storage','storage-3');
  });
 
 
  // click event for delevery unit
 
  document.getElementById('delevery-1').addEventListener('click',function(){
-    Delevery('delevery-1');
+    computer_confure('delevery','delevery-1');
   });
   document.getElementById('delevery-2').addEventListener('click',function(){
-   Delevery('delevery-2');
+   computer_confure('delevery','delevery-2');
  });
-        
+     
  
- /********************* 
+function computer_confure(type,quality){
+              const storageCost = document.getElementById(type+'-cost');
+              const storageCostText =storageCost.innerText; 
+              const storageCostamount =parseInt(storageCostText);
+
+              console.log(storageCostamount);
+
+            //for memory
+              if(quality =='8'){
+                storageCost.innerText =0;
+              }
+              else if(quality=='16')
+              {
+                storageCost.innerText =180;
+              }
+
+              //for storage
+
+              if(quality=='storage-1')
+              {
+                storageCost.innerText =0;
+              }
+              else if(quality=='storage-2'){
+                storageCost.innerText =100;
+              }
+              else if(quality=='storage-3'){
+                storageCost.innerText =180;
+              }
+
+              //for delevery
+              if(quality=='delevery-1')
+              {
+                storageCost.innerText =0;
+              }
+              else if(quality=='delevery-2')
+              {
+                storageCost.innerText =20;
+              }
+}
+
+
+
+
+
+
+
+
+
+ 
+//  /********************* 
                          
-                function for memory
+//                 function for memory
    
-    ********************/ 
+//     ********************/ 
    
                 
-function memoryadd(type){
+// function memoryadd(type){
     
-    const mcost_catch_id =document.getElementById('memory-cost');
-    const mcost_catch_value = parseInt(mcost_catch_id.innerText);
+//     const mcost_catch_id =document.getElementById('memory-cost');
+//     const mcost_catch_value = parseInt(mcost_catch_id.innerText);
 
-    //value set into the total calculation
-    if(type=='8-gb')
-    {
+//     //value set into the total calculation
+//     if(type=='8-gb')
+//     {
 
-        mcost_catch_id.innerText = 0;
-    }
-    else{
-        mcost_catch_id.innerText = 180;
-    } 
+//         mcost_catch_id.innerText = 0;
+//     }
+//     else{
+//         mcost_catch_id.innerText = 180;
+//     } 
 
-}
+// }
 
-/**********************
-   storage calculation
-***********************/
+// /**********************
+//    storage calculation
+// ***********************/
 
-function ssdStorage(type){
+// function ssdStorage(type){
       
-    const storageCost = document.getElementById('storage-cost');
-    const storageCostText =storageCost.innerText; 
-    const storageCostamount =parseInt(storageCostText); 
+//     const storageCost = document.getElementById('storage-cost');
+//     const storageCostText =storageCost.innerText; 
+//     const storageCostamount =parseInt(storageCostText); 
 
-    if(type=='storage-1')
-    {
-        storageCost.innerText =0;
-    }
-    else if(type=='storage-2'){
+//     if(type=='storage-1')
+//     {
+//         storageCost.innerText =0;
+//     }
+//     else if(type=='storage-2'){
 
-        storageCost.innerText =100;
-    }
-    else
-    {
-        storageCost.innerText =180;
-    }
-}
+//         storageCost.innerText =100;
+//     }
+//     else
+//     {
+//         storageCost.innerText =180;
+//     }
+// }
 
 
-/**********************
-   Delevery calculation
-***********************/
-function Delevery(type){
+// /**********************
+//    Delevery calculation
+// ***********************/
+// function Delevery(type){
       
-    const deleveryCost = document.getElementById('delevery-cost');
-    const deleveryCostText =deleveryCost.innerText; 
-    const deleveryCostamount =parseInt(deleveryCostText); 
+//     const deleveryCost = document.getElementById('delevery-cost');
+//     const deleveryCostText =deleveryCost.innerText; 
+//     const deleveryCostamount =parseInt(deleveryCostText); 
 
-    if(type=='delevery-1')
-    {
-        deleveryCost.innerText =0;
-    }
-    else{
+//     if(type=='delevery-1')
+//     {
+//         deleveryCost.innerText =0;
+//     }
+//     else{
 
-        deleveryCost.innerText =20;
-    }
+//         deleveryCost.innerText =20;
+//     }
     
-}
+// }
